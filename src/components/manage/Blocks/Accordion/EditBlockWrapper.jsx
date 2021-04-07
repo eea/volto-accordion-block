0e37fb3542be53806017d0611c9239e5cdf94193
Alin Voinea <contact@avoinea.com>
2021-04-07 18:05:51 +0300

    Add support for onInsertBlock with BlockChooser

--- src/components/manage/Blocks/Accordion/EditBlockWrapper.jsx
+++ src/components/manage/Blocks/Accordion/EditBlockWrapper.jsx
@@ -72,7 +72,9 @@ class EditBlockWrapper extends React.Component {
       block,
       data,
       onDeleteBlock,
+      onInsertBlock,
       onMutateBlock,
+      onSelectBlock,
       selected,
     } = blockProps;
     const type = data['@type'];
@@ -148,8 +150,12 @@ class EditBlockWrapper extends React.Component {
                   {this.state.addNewBlockOpened && (
                     <BlockChooser
                       onMutateBlock={(id, value) => {
-                        this.setState({ addNewBlockOpened: false });
                         onMutateBlock(id, value);
+                        this.setState({ addNewBlockOpened: false });
+                      }}
+                      onInsertBlock={(id, value) => {
+                        onSelectBlock(onInsertBlock(id, value));
+                        this.setState({ addNewBlockOpened: false });
                       }}
                       currentBlock={block}
                       allowedBlocks={allowedBlocks}
