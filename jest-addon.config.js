7a1af55061b0c75ecb77fdbae9e2e6586df3b50c
nileshgulia1 <nileshgulia@gmail.com>
2022-08-23 14:03:33 +0530

    update(jest): add @plone/volto-slate resolver refs- #153447

--- jest-addon.config.js
+++ jest-addon.config.js
@@ -11,7 +11,8 @@ module.exports = {
     '@package/(.*)$': '<rootDir>/src/$1',
     '@plone/volto-quanta/(.*)$': '<rootDir>/src/addons/volto-quanta/src/$1',
     '@eeacms/(.*?)/(.*)$': '<rootDir>/src/addons/$1/src/$2',
-    'volto-slate/(.*)$': '<rootDir>/src/addons/volto-slate/src/$1',
+    '@plone/volto-slate':
+      '<rootDir>/node_modules/@plone/volto/packages/volto-slate/src',
     '~/(.*)$': '<rootDir>/src/$1',
     'load-volto-addons':
       '<rootDir>/node_modules/@plone/volto/jest-addons-loader.js',
