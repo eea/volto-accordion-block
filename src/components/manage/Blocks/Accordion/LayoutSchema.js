f05193c41010c7a888f6d6afbf39f727b8108d3d
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-09-27 10:27:23 +0200

    Fix anonymous default export

--- src/components/manage/Blocks/Accordion/LayoutSchema.js
+++ src/components/manage/Blocks/Accordion/LayoutSchema.js
@@ -63,8 +63,7 @@ const messages = defineMessages({
     defaultMessage: 'Fixed layout',
   },
   FixedlayoutNewPanesTabs: {
-    id:
-      'Fixed layout, New panes (tabs) created by Editor within this block will be ignored',
+    id: 'Fixed layout, New panes (tabs) created by Editor within this block will be ignored',
     defaultMessage:
       'Fixed layout, New panes (tabs) created by Editor within this block will be ignored',
   },
@@ -110,7 +109,7 @@ const messages = defineMessages({
   },
 });
 
-export default (intl) => ({
+const layoutSchema = (intl) => ({
   title: intl.formatMessage(messages.AccordionBlockSettings),
   fieldsets: [
     {
@@ -208,3 +207,5 @@ export default (intl) => ({
   },
   required: [],
 });
+
+export default layoutSchema;
