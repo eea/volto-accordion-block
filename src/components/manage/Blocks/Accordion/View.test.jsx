8ed6473a10e352cd40533ef26798dcad0abc6356
ana-oprea <anaop3212@gmail.com>
2024-03-22 11:31:05 +0200

    test: add unit tests and delete snapshots

--- src/components/manage/Blocks/Accordion/View.test.jsx
+++ src/components/manage/Blocks/Accordion/View.test.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import View from './View';
-import renderer from 'react-test-renderer';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-intl-redux';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 import config from '@plone/volto/registry';
 import * as utils from './util';
 import '@testing-library/jest-dom/extend-expect';
@@ -17,6 +16,14 @@ config.blocks.blocksConfig.accordion = {
       leftPosition: 'chevron right',
     },
     closed: {
+      rightPosition: 'chevron down',
+      leftPosition: 'chevron down',
+    },
+    unfiltered: {
+      rightPosition: 'chevron left',
+      leftPosition: 'chevron right',
+    },
+    filtered: {
       rightPosition: 'chevron down',
       leftPosition: 'chevron down',
     },
@@ -67,33 +74,35 @@ const mockData1 = {
   non_exclusive: true,
 };
 
+const mockData2 = {
+  data: {
+    blocks: {
+      1: {
+        '@type': 'accordionPanel',
+        title: 'Panel 1',
+        blocks_layout: {
+          items: ['block1', 'block2'],
+        },
+      },
+      2: {
+        '@type': 'accordionPanel',
+        title: 'Panel 2',
+        blocks_layout: {
+          items: ['block3', 'block4'],
+        },
+      },
+    },
+    blocks_layout: {
+      items: ['1', '2'],
+    },
+  },
+  right_arrows: false,
+  non_exclusive: true,
+  collapsed: false,
+  filtering: false,
+};
+
 describe('View Component', () => {
-  it('renders without crashing', () => {
-    utils.accordionBlockHasValue.mockReturnValue(true);
-    const component = renderer.create(
-      <Provider store={store}>
-        <MemoryRouter>
-          <View data={mockData} />
-        </MemoryRouter>
-      </Provider>,
-    );
-    const json = component.toJSON();
-    expect(json).toMatchSnapshot();
-  });
-
-  it('renders null', () => {
-    utils.accordionBlockHasValue.mockReturnValue(false);
-    const component = renderer.create(
-      <Provider store={store}>
-        <MemoryRouter>
-          <View data={mockData} />
-        </MemoryRouter>
-      </Provider>,
-    );
-    const json = component.toJSON();
-    expect(json).toMatchSnapshot();
-  });
-
   it('renders with panels', () => {
     utils.accordionBlockHasValue.mockReturnValue(true);
     const { rerender, getByText } = render(
@@ -162,5 +171,52 @@ describe('View Component', () => {
 
     const contentElement = getByText('RenderBlocks');
     expect(contentElement).toBeInTheDocument();
+  });
+
+  it('filters the accordion panels based on the filter value', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <View data={{ ...mockData2, filtering: true }} />
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    const filterInput = screen.getByPlaceholderText('Type to filter...');
+    fireEvent.change(filterInput, { target: { value: 'Panel 1' } });
+
+    expect(screen.getByText('Panel 1')).toBeInTheDocument();
+    expect(screen.queryByText('Panel 2')).not.toBeInTheDocument();
+  });
+
+  it('updates the URL query parameter when a panel is clicked', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/']}>
+          <View data={mockData2} />
+          <Route
+            path="*"
+            render={({ location }) => <div>{location.search}</div>}
+          />
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    const panel1Title = screen.getByText('Panel 1');
+    fireEvent.click(panel1Title);
+
+    expect(screen.getByText('?activeAccordion=')).toBeInTheDocument();
+  });
+
+  it('renders the accordion panels in diff view mode', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/diff']}>
+          <View data={mockData2} />
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    expect(screen.getByText('RenderBlocks')).toBeInTheDocument();
   });
 });
