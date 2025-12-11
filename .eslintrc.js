e8e0504a65b9167ca3836eb41fc58811d518f6e3
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-12-11 10:18:12 +0100

    Tentatively push a eslint config to catch the barrel imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,30 @@ const addonExtenders = reg.getEslintExtenders().map((m) => require(m));
 
 const defaultConfig = {
   extends: `${voltoPath}/.eslintrc`,
+  rules: {
+    'import/no-unresolved': 1,
+    'import/named': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+    'no-restricted-imports': [
+      'error',
+      {
+        name: '@plone/volto/components',
+        message:
+          'Importing from barrel files is not allowed. Please use direct imports of the modules instead.',
+      },
+      {
+        name: '@plone/volto/helpers',
+        message:
+          'Importing from barrel files is not allowed. Please use direct imports of the modules instead.',
+      },
+      {
+        name: '@plone/volto/actions',
+        message:
+          'Importing from barrel files is not allowed. Please use direct imports of the modules instead.',
+      },
+    ],
+    'react/jsx-key': [2, { checkFragmentShorthand: true }],
+  },
   settings: {
     'import/resolver': {
       alias: {
